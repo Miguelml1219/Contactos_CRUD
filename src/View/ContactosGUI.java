9c12b99e3d687820641801d87c24565da0a783e6
Miguelml1219 <miguel.medinaladino19@gmail.com>
2025-04-18 20:10:54 -0500

    Creacion BD

--- src/View/ContactosGUI.java
+++ src/View/ContactosGUI.java
@@ -1,4 +1,26 @@
 package View;
 
+import javax.swing.*;
+
 public class ContactosGUI {
+
+    private JPanel main;
+
+    public ContactosGUI(JFrame frame)
+    {
+
+    }
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Data Base Contact");
+        ContactosGUI contactosGUI = new ContactosGUI(frame); // Se pasa el frame al constructor de Menu
+        frame.setContentPane(contactosGUI.main);
+//          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(320,210);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
+
 }
